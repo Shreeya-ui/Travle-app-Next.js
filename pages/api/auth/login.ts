0b24532182b4deff0b01dc1d745b022a135879e7
Shreeya-ui <shreeya.b.kumbhar@gmail.com>
2025-03-08 12:24:42 +0530

    done

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -8,6 +8,8 @@ import { sendEmail } from "@/utils/sendEmails";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
+  }else{
+    res.status(200).json({ message: "Login successful" });
   }
 
   const { email, password } = req.body;
